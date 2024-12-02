e0cf215ca9e7637f9e018012fa52e78d3fe499a4
NaimoonJannat <naimoonjannat@gmail.com>
2024-12-02 23:41:11 +0600

    Navbar done

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
    <main>
+  <Navbar />
 
+
+  <Footer />
 
    </main>
   );
