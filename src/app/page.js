bda5ceea62bebf96b91b70039159868bebddfba1
NaimoonJannat <naimoonjannat@gmail.com>
2024-12-03 00:07:42 +0600

    Banner done

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Banner from "@/components/Banner/Banner";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 
@@ -5,7 +6,7 @@ export default function Home() {
   return (
    <main>
   <Navbar />
-
+ <Banner />
 
   <Footer />
 
